0ec82a3f136f8044f5808cec0c1c21e2fb3bc878
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2022-11-22 13:00:52 +0600

    cloudinit config

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "cloudfront_distribution_domain_name" {
-    description = "Distribution domain name"
-    value       = aws_cloudfront_distribution.destination_distribution.cloudfront_distribution_domain_name
-}
+# output "url" {
+#     description = "Distribution domain name"
+#     value       = aws_cloudfront_distribution.destination_distribution.cloudfront_distribution_domain_name
+# }
--- Terraform/setup.sh
+++ Terraform/setup.sh
@@ -4,18 +4,19 @@ touch configure
 
 # while ! apt install -y ffmpeg; do sleep 5; done;
 
-# sudo apt update && sudo apt upgrade -y
-# echo "done"
-# sudo apt-get install ffmpeg -y
-# sudo apt  install awscli -y
+apt update && sudo apt upgrade -y
+echo "done"
+apt-get install ffmpeg -y
+apt  install awscli -y
 
-# sudo apt install python3-pip -y
-# pip3 install boto3
+apt install python3-pip -y
+pip3 install boto3
 
 # cd /root
 
-# sudo wget https://github.com/shaka-project/shaka-packager/releases/download/v2.6.1/packager-linux-x64  -O /bin/packager
-# sudo chmod +x /bin/packager
+wget https://github.com/shaka-project/shaka-packager/releases/download/v2.6.1/packager-linux-x64  -O ../bin/packager
+chmod +x ../bin/packager
 
 echo "for faster/visual confirmation of above execution.."
 wget https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4 -O I_RAN.mp4
+# wget https://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4 -O I_RAN2.mp4
